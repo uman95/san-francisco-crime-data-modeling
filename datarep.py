5f1a37d57245b5ae49814a765337ded47d2e8fd3
Eltayeb Ahmed <eltayeb.k.ahmed@gmail.com>
2018-11-27 14:05:09 +0200

    Fixed documentation

--- datarep.py
+++ datarep.py
@@ -112,7 +112,14 @@ def load_dataset(path_to_dataset, time_of_day=True, weekend_flag=True, normalize
     """Return a tuple of two numpy arrays. Input features and target.
 
     Input features has N rows and the following columns:
-    Unix_time (1 columns),[Time_of_day (3 columns onehot)], Day_of_week (7 columns onehot), [Is_weekend (1 column)], District (10 columns onehot), geoloc (2 columns)
+    Unix_time (1 columns)
+    [Time_of_day (3 columns onehot)]
+    Day_of_week (7 columns onehot)
+    [Is_weekend (1 column)]
+    District (10 columns onehot)
+    geoloc (2 columns (long, lat))
+
+    The columns in square brackets will be abscent if their respective flags are set to False
 
     Target is a one hot encoding (dimension N x 39)
 
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 This repository is the work for a group project done at the African Masters of Machine Intelligence (AMMI, AIMS Rwanda). 
 The project is to model and predict crime in the San Francisco area using the Kaggle dataset.
 
-###Code documenttion
-[a](https://eltayebahmed.github.io/san-francisco-crime-data-modeling/datarep.m.html)
+### Code documenttion
+[Data loading functions](https://eltayebahmed.github.io/san-francisco-crime-data-modeling/datarep.m)

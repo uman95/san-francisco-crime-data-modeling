4519ed276710c34ee4540d79c917e2c77c83217e
Eltayeb Ahmed <eltayeb.k.ahmed@gmail.com>
2018-11-27 13:57:48 +0200

    Added documentation

--- datarep.py
+++ datarep.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 
-def getDates(dates):
+def _get_dates(dates):
     yy = int(dates[:4])
 
     # print ("year = ",yy)
@@ -32,14 +32,14 @@ def getDates(dates):
     return yy, mm, dd, hh, minutes, seconds
 
 
-def getUnixTime(time_as_tuple):
+def _get_unix_time(time_as_tuple):
     timer = datetime.datetime(*time_as_tuple)
     timer = time.mktime(timer.timetuple())
     return timer
 
 
 # Get the moment time the crime was happening
-def get_time_of_day(time_as_tuple):
+def _get_time_of_day(time_as_tuple):
     # moment = "Morning"
     hh = time_as_tuple[3]
     if hh >= 6 and hh < 13:
@@ -54,11 +54,11 @@ def get_time_of_day(time_as_tuple):
     return moment
 
 
-def parse_data_to_onehot_encoding(datas_int, n_uniques):
+def _parse_data_to_onehot_encoding(datas_int, n_uniques):
     data_one_hot = np.eye(n_uniques)[datas_int]
     return data_one_hot
 
-def encode_date_time(date_time, time_of_day=True):
+def _encode_date_time(date_time, time_of_day=True):
     n = int(len(date_time))
     offset = datetime.datetime(2003, 1, 1, 0, 0, 0) # start of data collection
     offset = time.mktime(offset.timetuple()) # to prevent possible numerical overflow
@@ -68,15 +68,15 @@ def encode_date_time(date_time, time_of_day=True):
         date_time_mat = np.zeros((n, 1), dtype=np.float64)
 
     for i, entry in enumerate(date_time):
-        tuple_time = getDates(entry)
-        unix_time = getUnixTime(tuple_time) - offset
+        tuple_time = _get_dates(entry)
+        unix_time = _get_unix_time(tuple_time) - offset
         date_time_mat[i, 0] = unix_time
         if time_of_day:
-            x = get_time_of_day(tuple_time)
+            x = _get_time_of_day(tuple_time)
             date_time_mat[i, 1] = x
     return date_time_mat
 
-def one_hot_encode_strings(input_array):
+def _one_hot_encode_strings(input_array):
     uniques = list(np.unique(input_array))
     uniques.sort()
     n = len(input_array)
@@ -84,23 +84,23 @@ def one_hot_encode_strings(input_array):
     n = len(input_array)
     for i in range(n):
         int_encoding[i] = uniques.index(input_array[i])
-    return parse_data_to_onehot_encoding(int_encoding, len(uniques))
+    return _parse_data_to_onehot_encoding(int_encoding, len(uniques))
 
 
 
-def design_matrix(pandas_frame, time_of_day=True, weekend_flag=True, normalized=True):
+def _design_matrix(pandas_frame, time_of_day=True, weekend_flag=True, normalized=True):
     n = int(len(pandas_frame))
-    date = encode_date_time(pandas_frame["Dates"], time_of_day)
-    day_of_week = one_hot_encode_strings(pandas_frame["DayOfWeek"])
+    date = _encode_date_time(pandas_frame["Dates"], time_of_day)
+    day_of_week = _one_hot_encode_strings(pandas_frame["DayOfWeek"])
     if weekend_flag:
         is_weekend = (day_of_week[:, 2] == 1) | (day_of_week[:, 3] == 1)
         day_of_week = np.hstack((day_of_week, is_weekend.reshape(-1, 1)))
 
 
-    district = one_hot_encode_strings(pandas_frame["PdDistrict"])
+    district = _one_hot_encode_strings(pandas_frame["PdDistrict"])
     geoloc = np.hstack((np.array(pandas_frame["X"]).reshape(-1,1),
                         np.array(pandas_frame["Y"]).reshape(-1,1)))
-    crime = one_hot_encode_strings(pandas_frame["Category"])
+    crime = _one_hot_encode_strings(pandas_frame["Category"])
 
     if normalized:
         date[:, 0] = (date[:, 0] - date[:, 0])/date[:, 0].std()
@@ -109,9 +109,19 @@ def design_matrix(pandas_frame, time_of_day=True, weekend_flag=True, normalized=
     return features, crime
 
 def load_dataset(path_to_dataset, time_of_day=True, weekend_flag=True, normalized=True):
-    """Return a tuple of numpy arrays"""
+    """Return a tuple of two numpy arrays. Input features and target.
+
+    Input features has N rows and the following columns:
+    Unix_time (1 columns),[Time_of_day (3 columns onehot)], Day_of_week (7 columns onehot), [Is_weekend (1 column)], District (10 columns onehot), geoloc (2 columns)
+
+    Target is a one hot encoding (dimension N x 39)
+
+    ----------
+
+    Example Usage:
+    input_array, target = load_dataset("path/to/file/train.csv")"""
     data = pd.read_csv(path_to_dataset)
-    return design_matrix(data, time_of_day=time_of_day, weekend_flag=weekend_flag, normalized=normalized)
+    return _design_matrix(data, time_of_day=time_of_day, weekend_flag=weekend_flag, normalized=normalized)
 
 
 if __name__ == "__main__":
---
+++ readme.md
@@ -0,0 +1,5 @@
+This repository is the work for a group project done at the African Masters of Machine Intelligence (AMMI, AIMS Rwanda). 
+The project is to model and predict crime in the San Francisco area using the Kaggle dataset.
+
+###Code documenttion
+[a](https://eltayebahmed.github.io/san-francisco-crime-data-modeling/datarep.m.html)
